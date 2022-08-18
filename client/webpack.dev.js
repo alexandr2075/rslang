929ac8291ff9dc68c5cf78f1feb6aa74e6b24fac
alexandr2075 <gagara5620@gmail.com>
2022-08-18 11:25:10 +0600

    init: add configs

---
+++ client/webpack.dev.js
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    static: './dist',
+  },
+});
---
+++ client/webpack.prod.js
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+// eslint-disable-next-line import/no-extraneous-dependencies
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+});
