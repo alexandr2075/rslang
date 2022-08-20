f666045bdf4325bc853c151d8d21d48b22056187
ephedrini1 <ephedrini@yandex.ru>
2022-08-20 19:29:00 +0300

    feat: add HTML-component template

---
+++ client/src/utils/createComponent.js
@@ -0,0 +1,16 @@
+export default class createComponent {
+
+  constructor(parentNode, tagName = '', className = '', content = '') {
+    const element = document.createElement(tagName);
+    element.className = className;
+    element.textContent = content;
+    if (parentNode) {
+      parentNode.append(element);
+    }
+    this.node = element;
+  }
+
+   destroy() {
+    this.node.remove();
+  }
+}
