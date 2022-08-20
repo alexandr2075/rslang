f60c8f9d905b90d2e60fc857e571e8b0f369a9b1
ephedrini1 <ephedrini@yandex.ru>
2022-08-20 13:47:53 +0300

    refactor: wordsApi.js file & add request

--- client/src/api/wordsApi.js
+++ client/src/api/wordsApi.js
@@ -1,16 +1,15 @@
 import { Api } from "../utils/api";
-export default class WordsApi {
-   async getWords(){
-    try {
-      const response = await fetch(`${Api.baseUrl}/${Api.endpoints.words}?${Api.queries.words.page}=2&${Api.queries.words.group}=0`);
-      return {
-        data: await response.json(),
-      };
-    }
-    catch(error) {
-      throw new Error(error.message);
-     }
-  
- }
+const getWords = async (page = 0, group = 0) => {
+  const url = `${Api.baseUrl}/${Api.endpoints.words}?${Api.queries.words.page}=${page}&${Api.queries.words.group}=${group}`
+  return (await fetch(url)).json();
+}
 
+const getWordById = async (wordId) => {
+  const url = `${Api.baseUrl}/${Api.endpoints.words}/${wordId}`
+  return (await fetch(url)).json();
+}
+
+export  {
+  getWords,
+  getWordById,
 }
