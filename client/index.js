ca732db99c33838bf1c8a97b1ad508aba21fefb3
alexandr2075 <gagara5620@gmail.com>
2022-08-20 19:46:24 +0600

    feat: merge with develop

--- client/index.js
+++ client/index.js
@@ -1,8 +1,12 @@
 import './style.css';
-import WordsApi from "./src/api/wordsApi";
+import WordsApi from './src/api/wordsApi';
+import Auth from './src/auth';
 
 const app = document.querySelector('#app');
 app.innerText = 'Learnwords';
 
-const words = new WordsApi();
-console.log(words.getWords())
+// const words = new WordsApi();
+console.log(WordsApi.getWords());
+
+const auth = new Auth();
+auth.render();
--- client/src/api/wordsApi.js
+++ client/src/api/wordsApi.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Api } from "../utils/api";
 const getWords = async (page = 0, group = 0) => {
   const url = `${Api.baseUrl}/${Api.endpoints.words}?${Api.queries.words.page}=${page}&${Api.queries.words.group}=${group}`
@@ -12,4 +13,19 @@ const getWordById = async (wordId) => {
 export  {
   getWords,
   getWordById,
+=======
+import Api from '../utils/api';
+
+export default class WordsApi {
+  static async getWords() {
+    try {
+      const response = await fetch(`${Api.baseUrl}/${Api.endpoints.words}?${Api.queries.words.page}=2&${Api.queries.words.group}=0`);
+      return {
+        data: await response.json(),
+      };
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+>>>>>>> d30279d (feat: add auth)
 }
