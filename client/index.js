91fb94f40609ed2efb932b98490c09d55b6d9c1d
alexandr2075 <gagara5620@gmail.com>
2022-08-23 15:57:26 +0600

    feat: update to develop

--- client/index.js
+++ client/index.js
@@ -1,5 +1,7 @@
+import './style.css';
 import out from './src/js/MainPage';
 import WordsApi from './src/api/wordsApi';
+import Auth from './src/auth';
 
 const app = document.querySelector('#app');
 app.innerText = 'Learnwords';
@@ -7,5 +9,9 @@ app.innerText = 'Learnwords';
 const main = out();
 app.insertAdjacentHTML('beforeend', main);
 
-const words = new WordsApi();
-console.log(words.getWords());
+// const words = new WordsApi();
+console.log(WordsApi.getWords());
+
+const auth = new Auth();
+auth.render();
+
