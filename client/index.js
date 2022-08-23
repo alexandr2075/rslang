ae9d8542b291c5a9d874ef9fad6eace1467fb5cf
ephedrini1 <ephedrini@yandex.ru>
2022-08-23 21:29:54 +0300

    fix:rebase conflicts

--- client/index.js
+++ client/index.js
@@ -1,11 +1,8 @@
-import out from './src/js/MainPage';
-import WordsApi from './src/api/wordsApi';
+import './style.css';
+import WordsApi from "./src/api/wordsApi";
 
 const app = document.querySelector('#app');
 app.innerText = 'Learnwords';
 
-const main = out();
-app.insertAdjacentHTML('beforeend', main);
-
 const words = new WordsApi();
-console.log(words.getWords());
+console.log(words.getWords())
