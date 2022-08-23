37be156c3198a367af0c10c2b59cfe4d452d1a13
ephedrini1 <ephedrini@yandex.ru>
2022-08-23 19:01:09 +0300

    feat: update state

--- client/src/utils/state.js
+++ client/src/utils/state.js
@@ -1,6 +1,8 @@
 const wordsPageState = {
   page: 1,
-  group: 0,
-}
+  levels: ['A1 elementary', 'A2 pre-intermediate', 'B1 intermediate', 'B2 upper-intermediate', 'C1 advanced', 'C2 proficiency'],
+  group: 0
+  }
+
 
 export default wordsPageState;
