ce6dfe39b737f62052496d190f8ef0a0eee5f11b
ephedrini1 <ephedrini@yandex.ru>
2022-08-24 01:49:28 +0300

    feat: add color-state for dictionary

--- client/src/utils/state.js
+++ client/src/utils/state.js
@@ -1,7 +1,15 @@
 const wordsPageState = {
   page: 1,
   levels: ['A1 elementary', 'A2 pre-intermediate', 'B1 intermediate', 'B2 upper-intermediate', 'C1 advanced', 'C2 proficiency'],
-  group: 0
+  group: 0,
+  color: {
+    0: '#a867fd',
+    1: '#00e1ff',
+    2: '#73fd67',
+    3: '#f1ff72',
+    4: '#ffce72',
+    5: '#ff85a3',
+  }
   }
 
 
