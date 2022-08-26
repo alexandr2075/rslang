0f151c14b6472eb665075ca5ecfd8d7f747b8d0e
alexandr2075 <gagara5620@gmail.com>
2022-08-26 09:21:03 +0600

    l

--- client/index.js
+++ client/index.js
@@ -1,8 +1,10 @@
 import './style.css';
 import { getWords } from './src/api/wordsApi';
+import WordsPage from './src/components/dictionary/words-page/words-page';
 
 const app = document.querySelector('#app');
 app.innerText = 'Learnwords';
 
 // const words = new WordsApi();
 console.log(getWords());
+const wordsPage = new WordsPage(document.body)
