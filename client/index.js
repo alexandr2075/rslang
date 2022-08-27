23203f15f57002d98788c178be16d30a677ce547
alexandr2075 <gagara5620@gmail.com>
2022-08-27 18:58:42 +0600

    feat: clear index.js

--- client/index.js
+++ client/index.js
@@ -1,14 +1,3 @@
 import 'normalize.css';
 import './style.css';
-import Auth from './src/auth';
 
-const app = document.getElementById('app');
-const entry = document.createElement('button');
-entry.innerText = 'Entry';
-app.append(entry);
-const login = document.createElement('button');
-login.innerText = 'Login';
-app.append(login);
-
-entry.onclick = () => { const auth = new Auth(document.body, 'Регистрация'); };
-login.onclick = () => { const auth = new Auth(document.body, 'Войти'); };
