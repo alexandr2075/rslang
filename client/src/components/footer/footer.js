2eec534519c1848b72d61f873973736db7ea9003
ephedrini1 <ephedrini@yandex.ru>
2022-08-27 18:11:13 +0300

    feat: update footer info

--- client/src/components/footer/footer.js
+++ client/src/components/footer/footer.js
@@ -4,13 +4,22 @@ import './footer.scss'
 export default class Footer extends createComponent {
   constructor(parentNode) {
     super(parentNode, 'footer', 'footer', '');
-    this.renderInnerHTML();
+    this.render();
   }
 
-  renderInnerHTML() {
-    this.node.innerHTML = `
-      <a class="school" href="https://rs.school/js/">RSSchool
-      <span class="year">'2022</span>
-    </a>`;
+  render() {
+    this.rsSchoolLink = new createComponent(this.node, 'a', 'rss-link', 'rs-school');
+    this.rsSchoolLink.node.href = 'https://rs.school/js/';
+    this.developers = new createComponent(this.node, 'div', 'developers');
+    this.AlexGitHubLink = new createComponent(this.developers.node, 'a', 'rss-link')
+    this.AlexGitHubLink.node.innerHTML = `<i class="fa-brands fa-github"></i> Alexandr`;
+    this.AlexGitHubLink.node.href = 'https://github.com/alexandr2075';
+    this.VasyaGitHubLink = new createComponent(this.developers.node, 'a', 'rss-link');
+    this.VasyaGitHubLink.node.innerHTML = '<i class="fa-brands fa-github"></i> Vasya';
+    this.VasyaGitHubLink.node.href = 'https://github.com/Vasyaruberoid';
+    this.IrmaGitHubLink = new createComponent(this.developers.node, 'a', 'rss-link')
+    this.IrmaGitHubLink.node.innerHTML = '<i class="fa-brands fa-github"></i> Irma';
+    this.IrmaGitHubLink.node.href = 'https://github.com/ephedrini1';
+    this.year = new createComponent(this.node, 'span', 'year', "'22")
   }
 }
