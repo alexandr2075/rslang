70f4fa8af7c2f256e4e7ab2ddebe1accec7dcd93
Василий Старовойтов <vasiliy.starovojtov@mail.ru>
2022-08-27 12:21:07 +0300

    add:buttons

--- client/index.js
+++ client/index.js
@@ -1,11 +1,14 @@
-import out from './src/js/MainPage';
-import WordsApi from './src/api/wordsApi';
+import Header from "./src/components/Pages/MainPage/header";
+import MainPage from "./src/components/Pages/MainPage/MainPage";
+import Footer from "./src/components/Pages/MainPage/footer" ;
+import GamePage from "./src/components/Pages/GamePage/GamePage" ;
+import GameLinks from "./src/components/Pages/MainPage/linksGames"; 
 
-const app = document.querySelector('#app');
-app.innerText = 'Learnwords';
 
-const main = out();
-app.insertAdjacentHTML('beforeend', main);
 
-const words = new WordsApi();
-console.log(words.getWords());
+const header = new Header(document.body);
+const page = new MainPage(document.body);
+const footer = new Footer(document.body);
+const gamePage = new GamePage(document.body);
+const gameLinks = new GameLinks(document.body);
+
