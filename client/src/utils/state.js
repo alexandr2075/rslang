c629e6f054e564a073204e1eb9c60aadc4dc9163
ephedrini1 <ephedrini@yandex.ru>
2022-08-27 18:08:26 +0300

    feat: update state

--- client/src/utils/state.js
+++ client/src/utils/state.js
@@ -2,6 +2,7 @@ const wordsPageState = {
   page: 1,
   levels: ['A1 elementary', 'A2 pre-intermediate', 'B1 intermediate', 'B2 upper-intermediate', 'C1 advanced', 'C2 proficiency'],
   group: 0,
+  showTranslate: true,
   color: {
     0: '#a867fd',
     1: '#00e1ff',
