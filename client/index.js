d903bf63333ebc14b9153edda7695b501536cbd4
alexandr2075 <gagara5620@gmail.com>
2022-08-27 12:57:46 +0600

    feat: finish auth

--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import 'normalize.css';
 import './style.css';
-
 import Auth from './src/auth';
 
 const app = document.getElementById('app');
-const auth = new Auth(app);
+const entry = document.createElement('button');
+entry.innerText = 'Entry';
+app.append(entry);
+const login = document.createElement('button');
+login.innerText = 'Login';
+app.append(login);
+
+entry.onclick = () => { const auth = new Auth(document.body, 'Регистрация'); };
+login.onclick = () => { const auth = new Auth(document.body, 'Войти'); };
--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -5,51 +5,93 @@ import { createNewUser } from '../api/usersApi';
 import signInApi from '../api/signInApi';
 
 export default class Auth extends createComponent {
-  constructor(parentNode) {
+  constructor(parentNode, title) {
     super(parentNode, 'div', 'modal');
+    this.title = title;
     this.renderInnerHTML();
-    Auth.setEventListener();
+    this.setEventListener();
   }
 
   renderInnerHTML() {
     this.node.innerHTML = `
 		<div class='modal__wrapper'>
 			<div class='modal__form'>
-			<span class='reg' data-reg>Регистрация</span><span class='auth' data-auth>Авторизация</span>
-				<form class='form' id='auth-form'>
-				<div class="email">
-					<label for="email">Email</label>
-					<input id="email" type="email" required>
-				</div>
-				<div class="password">
-					<label for="password">Пароль</label>
-					<input id="password" type="password" required>
-				</div>
-				<button type="submit" class="sign-up" data-submit>Submit</button>
-				<p class='message'></p>
+			  <p class='reg'>${this.title}</p>
+				<form class='form' id='auth-form' onsubmit="return false;">
+          <div class="email">
+            <label for="email">Email</label>
+            <input id="email" type="email" required>
+          </div>
+          <div class="password">
+            <label for="password">Пароль</label>
+            <input id="password" type="password" required>
+          </div>
+          <button type="submit" class="sign-up" data-submit>Submit</button>
+          <div class='message'><div class="loader"></div></div>
 				</form>
 			</div>
 		</div>
 	  `;
 	  }
 
-  static setEventListener() {
-    const submit = document.querySelector('.sign-up');
-    submit.addEventListener('click', Auth.authFormHandler);
+  static textForUser(text) {
+    const message = document.querySelector('.message');
+    message.innerText = text;
   }
 
-  static authFormHandler(event) {
-    event.preventDefault();
-
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-
+  static getId(email, password) {
+    const loader = document.querySelector('.loader');
+    loader.style.opacity = '1';
     createNewUser({ email, password })
-      .then((data) => localStorage.setItem('idAndEmail', JSON.stringify(data)));
-    signInApi({ email, password })
-      .then((dataToken) => localStorage.setItem('token', JSON.stringify(dataToken)));
+      .then((data) => {
+        if (data) {
+          loader.style.opacity = '0';
+          localStorage.setItem('idAndEmail', JSON.stringify(data));
+          Auth.textForUser('Вы успешно зарегистрировались.');
+        }
+      }, (err) => {
+        Auth.textForUser(err);
+      });
+  }
 
-    const message = document.querySelector('.message');
-    message.innerText = 'Вы успешно зарегистрировались.';
+  getToken(email, password) {
+    signInApi({ email, password })
+      .then((dataToken) => {
+        if (dataToken) {
+          localStorage.setItem('token', JSON.stringify(dataToken));
+          Auth.textForUser('Успешный вход!');
+          setTimeout(() => {
+            this.destroy();
+          }, 1500);
+        }
+      }, (err) => {
+        Auth.textForUser(err);
+      });
+  }
+
+  authFormHandler(email, password) {
+    this.getId(email, password);
+    setTimeout(() => {
+      this.getToken(email, password);
+    }, 2000);
+  }
+
+  setEventListener() {
+    this.node.onclick = (event) => {
+      const { target } = event;
+      if (target.dataset.submit !== undefined) {
+        const email = document.querySelector('#email').value;
+        const password = document.querySelector('#password').value;
+        if (this.title === 'Регистрация') {
+          this.authFormHandler(email, password);
+        }
+        if (this.title === 'Войти') {
+          this.getToken(email, password);
+        }
+      }
+      if (!target.closest('.modal__form')) {
+        this.destroy();
+      }
+    };
   }
 }
