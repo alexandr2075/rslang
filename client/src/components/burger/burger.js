1265f62e7a297e8e3b44d8ebe3121812e2280907
ephedrini1 <ephedrini@yandex.ru>
2022-09-03 21:53:36 +0300

    feat: set routing

--- client/src/components/burger/burger.js
+++ client/src/components/burger/burger.js
@@ -1,8 +1,5 @@
 import './burger.scss';
 import CreateComponent from '../../utils/createComponent';
-import WordsMenu from '../dictionary/words-menu/words-menu';
-import Sprint from '../../games/sprint/sprint';
-import Header from '../main-page/header/header';
 
 export default class Burger extends CreateComponent {
   constructor(parentNode) {
@@ -17,28 +14,78 @@ export default class Burger extends CreateComponent {
 
   renderDropDownMenu() {
     this.menuBurger = new CreateComponent(document.body, 'div', 'drop-down-menu');
-    this.menuBurger.node.innerHTML = `
-		<div>
-			<nav>
-				<ul>
-					<li class='burger burger-main'><a href="#main">Главная</a></li>
-					<li class='burger burger-dictionary'><a href="/posts">Учебник</a></li>
-					<li class='burger burger-stat'><a href="#statistics">Статистика</a></li>
-					<li class='burger burger-audiocall'><a href="#audiocall">Аудиовызов</a></li>
-					<li class='burger burger-sprint'><a href="#sprint">Спринт</a></li>
-					<li class='burger burger-about-team'><a href="#about-team">О команде</a></li>
-				</ul>
-			</nav>
-		</div>
-	  `;
+    this.menu = new CreateComponent(this.menuBurger.node, 'div');
+    this.navigation = new CreateComponent(this.menu.node, 'nav');
+    this.menuList = new CreateComponent(this.navigation.node, 'ul');
+    
+    this.menuItemMain = new CreateComponent(this.menuList.node, 'li', 'burger burger-main', 'Главная');
+    this.menuItemMain.node.setAttribute('data-menu', 'main');
+   // this.menuItemMain.node.innerHTML = `<a href="#main" data-menu="main">Главная</a>`
+
+    this.menuItemDictionary = new CreateComponent(this.menuList.node, 'li', 'burger burger-dictionary', 'Учебник');
+    this.menuItemDictionary.node.setAttribute('data-menu', 'dictionary');
+   // this.menuItemDictionary.node.innerHTML = `<a href="/posts" data-menu="dictionary">Учебник</a>`
+
+    this.menuItemStat = new CreateComponent(this.menuList.node, 'li', 'burger burger-stat', 'Статистика');
+    this.menuItemStat.node.setAttribute('data-menu', 'stat');
+   // this.menuItemStat.node.innerHTML = `<a href="#statistics" data-menu="stat">Статистика</a>`
+
+    this.menuItemAudiocall = new CreateComponent(this.menuList.node, 'li', 'burger burger-audiocall');
+    this.menuItemAudiocall.node.setAttribute('data-menu', 'audiocall', 'Аудиовызов');
+   // this.menuItemAudiocall.node.innerHTML = `<a href="#audiocall" data-menu="audiocall">Аудиовызов</a>`
+    
+    this.menuItemSprint = new CreateComponent(this.menuList.node, 'li', 'burger burger-sprint', 'Спринт');
+    this.menuItemSprint.node.setAttribute('data-menu', 'sprint');
+   // this.menuItemSprint.node.innerHTML = `<a href="#sprint" data-menu="sprint">Спринт</a>`
+    
+    this.menuItemAboutTeam = new CreateComponent(this.menuList.node, 'li', 'burger burger-about-team', 'О команде');
+    this.menuItemAboutTeam.node.setAttribute('data-menu', 'about-team');
+   // this.menuItemAboutTeam.node.innerHTML = `<a href="#about-team" data-menu="about-team">О команде</a>`
+    
+    // this.menuBurger.node.innerHTML = `
+		// <div>
+		// 	<nav>
+		// 		<ul>
+		// 			<li class='burger burger-main'><a href="#main">Главная</a></li>
+		// 			<li class='burger burger-dictionary'><a href="/posts">Учебник</a></li>
+		// 			<li class='burger burger-stat'><a href="#statistics">Статистика</a></li>
+		// 			<li class='burger burger-audiocall'><a href="#audiocall">Аудиовызов</a></li>
+		// 			<li class='burger burger-sprint'><a href="#sprint">Спринт</a></li>
+		// 			<li class='burger burger-about-team'><a href="#about-team">О команде</a></li>
+		// 		</ul>
+		// 	</nav>
+		// </div>
+	  // `;
 	  }
 
 	  setEventListener() {
     this.node.onclick = () => {
       this.menuBurger.node.classList.toggle('active');
     };
-    this.menuBurger.node.onclick = () => {
+    this.menuBurger.node.onclick = (event) => {
+      console.log(event.target)
       this.menuBurger.node.classList.toggle('active');
+      console.log(event.target.dataset.menu)
+      switch(event.target.dataset.menu) {
+        case 'dictionary' :
+          this.onDictionary();
+           break;
+      case 'main' :
+        this.onMain();
+        break;
+      case 'stat' :
+        this.onStat();
+        break;
+      case 'audiocall' :
+          this.onAudioCall();
+          break;
+      case 'sprint' :
+          this.onSprint();
+          break;
+      case 'about-team' :
+            this.onTeam();
+            break;
     };
   }
+}
 }
--- client/src/components/main-page/homePageBuilder.js
+++ client/src/components/main-page/homePageBuilder.js
@@ -3,12 +3,28 @@ import Footer from "./footer/footer";
 import Header from "./header/header";
 import Main from "./main/main";
 import './homePageBuilder.scss';
+import WordsPage from "../dictionary/words-page/words-page";
 
 export default class HomePageBuilder extends CreateComponent {
 	constructor(parentNode) {
 		super(parentNode, 'div', 'home-page-builder');
 		this.header = new Header(this.node);
-		this.main = new Main(this.node);
-		this.footer = new Footer(this.node);
+		this.menuHandler();
+		this.container = new CreateComponent(this.node, 'div', 'container');
+		this.main = new Main(this.container.node);
+		this.footer = new Footer(this.container.node);
+	}
+  
+	menuHandler() {
+		this.header.burger.onDictionary = async() => {
+			this.container.destroy();
+			this.container = new CreateComponent(this.node, 'div', 'container');
+			this.wordPage = new WordsPage(this.container.node, 'main', 'main')
+		}
+		this.header.burger.onMain = async() => {
+			this.container.destroy();
+			this.container = new CreateComponent(this.node, 'div', 'container');
+			this.main = new Main(this.container.node, 'main', 'main')
+		}
 	}
 }
