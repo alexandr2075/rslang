2f79bde68b0dff583acafe6d2d5d1da0db4a2355
ephedrini1 <ephedrini@yandex.ru>
2022-09-04 02:27:20 +0300

    feat: add Sprint link

--- client/src/components/main-page/homePageBuilder.js
+++ client/src/components/main-page/homePageBuilder.js
@@ -4,13 +4,14 @@ import Header from "./header/header";
 import Main from "./main/main";
 import './homePageBuilder.scss';
 import WordsPage from "../dictionary/words-page/words-page";
+import Sprint from "../../games/sprint/sprint";
 
 export default class HomePageBuilder extends CreateComponent {
 	constructor(parentNode) {
 		super(parentNode, 'div', 'home-page-builder');
 		this.header = new Header(this.node);
 		this.menuHandler();
-		this.container = new CreateComponent(this.node, 'div', 'container');
+		this.container = new CreateComponent(this.node, 'div', 'page-container');
 		this.main = new Main(this.container.node);
 		this.footer = new Footer(this.container.node);
 	}
@@ -18,13 +19,18 @@ export default class HomePageBuilder extends CreateComponent {
 	menuHandler() {
 		this.header.burger.onDictionary = async() => {
 			this.container.destroy();
-			this.container = new CreateComponent(this.node, 'div', 'container');
+			this.container = new CreateComponent(this.node, 'div', 'page-container');
 			this.wordPage = new WordsPage(this.container.node, 'main', 'main')
 		}
 		this.header.burger.onMain = async() => {
 			this.container.destroy();
-			this.container = new CreateComponent(this.node, 'div', 'container');
+			this.container = new CreateComponent(this.node, 'div', 'page-container');
 			this.main = new Main(this.container.node, 'main', 'main')
+		}
+		this.header.burger.onSprint = async() => {
+			this.container.destroy();
+			this.container = new CreateComponent(this.node, 'div', 'page-container');
+			this.main = new Sprint(this.container.node, 'main', 'main')
 		}
 	}
 }
