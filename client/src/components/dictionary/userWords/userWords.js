6cd2d64a381415f2e1a307aea73419d5223b35d5
ephedrini1 <ephedrini@yandex.ru>
2022-09-04 14:30:54 +0300

    refactor: delete logs

--- client/src/components/dictionary/userWords/userWords.js
+++ client/src/components/dictionary/userWords/userWords.js
@@ -7,34 +7,32 @@ export default class UserWords extends createComponent {
   constructor(parentNode) {
     super(parentNode, 'div', 'words', '');
     this.user = JSON.parse(localStorage.getItem('idAndEmail'));
-
     this.wordsRender();
   }
 
   async wordsRender() {
-    console.log(this.user)
     const data = await getAllUserWords(this.user.id).then(res => res.json())
     this.wordsContainer = new createComponent(this.node, 'div', 'user-words__container')
     if (data && data.length) {
       const newData = data.filter(item => item.difficulty === 'hard');
-      console.log(newData)
       newData.forEach(item => {
         this.word = new UserWord(this.wordsContainer.node, item.optional.wordData, item.id);
+        const btn = this.word.wordBtnBlock.node.childNodes[0].firstChild;
+          btn.classList.add('active');
+          btn.style.color = '#ffffff';
         this.userBtnsHandler(item);
       })
     }
   }
 
   async userBtnsHandler(wordData) {
-    console.log(wordData)
     this.id = wordData.id;
     this.wordId = wordData.wordId;
     const word = {
       difficulty: "easy",
     }
     this.word.wordBtnBlock.onDifficult = async () => {
-      console.log(await updateUserWordById(this.user.id, this.wordId, word));
-      console.log(await getAllUserWords(this.user.id).then(res => res.json()));
+      await updateUserWordById(this.user.id, this.wordId, word);
       this.rerenderWords();
     }
 
