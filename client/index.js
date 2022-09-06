dfd8e147b32927ce78e2f1b1dc6164753f82b1da
alexandr2075 <gagara5620@gmail.com>
2022-09-06 10:18:13 +0300

    Update index.js

--- client/index.js
+++ client/index.js
@@ -1,9 +1,6 @@
-// import 'normalize.css/';
-// import HomePageBuilder from './src/components/main-page/homePageBuilder';
-import Auth from './src/auth/auth';
-import AudioGame from './src/games/AudioCall/Audio';
+import 'normalize.css/';
+import HomePageBuilder from './src/components/main-page/homePageBuilder';
 
-// const BODY = document.body;
 const homePageBuilder = new AudioGame(document.body);
-// const auth = new Auth(document.body, 'Регистрация');
+
 
