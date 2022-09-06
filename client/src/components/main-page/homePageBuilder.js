31c8a50ea032268b64cd89ea910a89d5516657ed
ephedrini1 <ephedrini@yandex.ru>
2022-09-06 12:22:55 +0300

    feat: adaudio-links

--- client/src/components/main-page/homePageBuilder.js
+++ client/src/components/main-page/homePageBuilder.js
@@ -7,6 +7,7 @@ import Main from './main/main';
 import WordsPage from '../dictionary/words-page/words-page';
 import Sprint from '../../games/sprint/sprint';
 import Statistics from '../statistics/statistics';
+import AudioGame from '../../games/AudioCall/Audio';
 
 export default class HomePageBuilder extends CreateComponent {
   constructor(parentNode) {
@@ -40,7 +41,7 @@ export default class HomePageBuilder extends CreateComponent {
     this.header.burger.onAudioCall = async () => {
       this.container.destroy();
       this.container = new CreateComponent(this.node, 'div', 'common-container');
-      this.audiocall = new CreateComponent(this.container.node, 'div', 'au', 'Audiocall');
+      this.audiocall = new AudioGame(this.container.node);
     };
     this.header.burger.onSprint = async () => {
       this.container.destroy();
